f1751e91b266c0f3e71578cf8fa9a934f635e888
VesperTech <vespergaming23@gmail.com>
2025-12-15 20:09:49 -0500

    COnfigure Git LFS

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39f0b98d331f9fa983579977b2d88dd69ac38ff43c8ac1cb270abe1835155fdb
+oid sha256:85555251c12f68cdae8b6e009f10bb8bf92b2f404780ecc4da64b3f23a46f72e
 size 4407
