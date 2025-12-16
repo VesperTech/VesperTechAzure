bfdd48e8d891085b626d8b0ee04bdcda8e033f52
VesperTech <vespergaming23@gmail.com>
2025-12-16 08:26:10 -0500

    Commit made from VS Code Azure Static Web Apps

--- Provider.tf
+++ Provider.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdf5f22c2f632773b224bf103d41bb78ca47718a7e9076183bfcb47812f99784
-size 351
+oid sha256:f2cd885a19ba792ee8ef563b41242dde3eac76d56c81e101f5aa2a6cd5473bd8
+size 392
--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85555251c12f68cdae8b6e009f10bb8bf92b2f404780ecc4da64b3f23a46f72e
-size 4407
+oid sha256:d28102ef24b07a01491920fda983f99e6e1ec38e4833485cb938984ba884ccc7
+size 5310
