ca9c4639ee28a9ea5cc100b764a0b95305e1d2ae
VesperTech <vespergaming23@gmail.com>
2026-01-13 14:44:20 -0500

    fix index and config file

--- resume_website/vite.config.js
+++ resume_website/vite.config.js
@@ -4,16 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     sourcemap: false,
     minify: 'esbuild',
     rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          icons: ['lucide-react']
-        }
+      input: {
+        main: './index.html'
       }
     }
   },
