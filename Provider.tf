ebdef9b5bf2e6ecca9fa2640c135dc31e54d5283
VesperTech <vespergaming23@gmail.com>
2026-01-13 20:15:00 -0500

    fix lfs issue

--- Provider.tf
+++ Provider.tf
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0a8925c485ee075500934393030ecfeb84a58525689e65fc6a71b20b2c6d0fab
-size 353
+terraform {
+    required_providers{
+        azurerm={
+            source = "hashicorp/azurerm"
+            version = "4.6.0"
+        }
+    }
+}
+
+provider "azurerm" {
+  subscription_id = var.azure_subscription_id
+  tenant_id = var.azure_tenant_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  features {}
+}
