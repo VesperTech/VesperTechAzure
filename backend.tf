99613a19e6101ebbadd45f655ea4935e0e9c761e
VesperTech <vespergaming23@gmail.com>
2026-01-13 20:33:39 -0500

    remove files

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "VSP-TCH"
-    storage_account_name = "vesperstorage"
-    container_name       = "tfstate"
-    key                  = "vesperstorage/terraform.tfstate"
-  }
-
-}
